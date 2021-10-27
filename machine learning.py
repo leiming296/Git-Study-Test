54d35ca6eebedca35047467b1f83adfdfeb7177d
DESKTOP-S2EDBCG\ming <lei.ming296@gmail.com>
2021-10-27 14:01:32 -0700

    machine learn model

--- machine learning.py
+++ machine learning.py
@@ -1,8 +1,13 @@
 
 import sklearn
 
-#######  only example here 
+#######  only example here
 
 
 def print_number():
     print("ming test")
+
+
+def model_load():
+    model = 'machine learning model'
+    return model
