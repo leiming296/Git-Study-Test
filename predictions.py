10bf1f7e0d82365bfd53deb4edebce5479fa70d6
DESKTOP-S2EDBCG\ming <lei.ming296@gmail.com>
2021-10-28 14:52:46 -0700

    another problem

--- predictions.py
+++ predictions.py
@@ -5,32 +5,7 @@
 "machine learning working now"
 "lei ming starting"
 
-def master_function(m):
-    print ('from latop master')
-    print('master commit 2')
-    return m 
 
-def m3_data(n):
-    print('from bug 2')
-    print('commit 2 bug2')
-    print('bug fix from develop ')
-    return n
-
-def m4_data(m):
-    print('from laptop')
-    print('fix problem')
-    print('problem m4 fix from laptop')
-    return m
-
-def m5_data(m):
-    print('from PC')
-    print('problem fix from master')
-    return m
-
-def m6_function(b):
-    print('from master')
-    print('problme solved here, please merge')
-    return b
 
 def ml_predictions(n):
     n = n + 1
@@ -46,8 +21,3 @@ def m3_data(n):
     print('from bug 2, from bug 2 branch')
     return n
 
-
-def m7_function(c):
-    print('from master function, change a little from laptop')
-    print ('this function from latop maybe wrong')
-    return c 
