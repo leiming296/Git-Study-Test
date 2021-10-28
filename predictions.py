ddb34aaa086324d0e73086189913c93940163075
DESKTOP-S2EDBCG\ming <lei.ming296@gmail.com>
2021-10-28 10:43:27 -0700

    bug2 commit 1

--- predictions.py
+++ predictions.py
@@ -9,7 +9,12 @@
 
 def ml_predictions(n):
     n = n + 1
+    print('from bug2')
     return n
 
 def ml_data(m):
-    return m 
+    return m
+
+def m3_data(n):
+    print('from bug 2')
+    return n 
