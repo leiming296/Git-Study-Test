559c5fdbd41d47ee85dd5a69de2e970559ab6e9c
Ming Lei <10444636+leiming296@users.noreply.github.com>
2021-10-28 11:21:32 -0700

    bug fix from laptop

--- predictions.py
+++ predictions.py
@@ -10,22 +10,22 @@
 def ml_predictions(n):
     n = n + 1
     print('from bug2')
-    print('from commit 2')
+    print('bug2 from commit 2')
     return n
 
 def ml_data(m):
     print(m, 'from bug 1')
-    print('commit 2')
+    print('bug1 commit 2')
     return m
 
 def m2_data(m, n):
     print(m+n, 'from bug 1')
-    print('commit 2')
+    print('bug1 commit 2')
     return m +n
-    print('from commit 2')
-    return m
+
 
 def m3_data(n):
     print('from bug 2')
+    print('commit 2 bug2')
     return n
 
