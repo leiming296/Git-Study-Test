1152020d908757e2b01a47b044e058f775672563
DESKTOP-S2EDBCG\ming <lei.ming296@gmail.com>
2021-10-28 10:44:40 -0700

    bug 2 commit 2

--- predictions.py
+++ predictions.py
@@ -10,11 +10,14 @@
 def ml_predictions(n):
     n = n + 1
     print('from bug2')
+    print('from commit 2')
     return n
 
 def ml_data(m):
+    print('from commit 2')
     return m
 
 def m3_data(n):
     print('from bug 2')
-    return n 
+    return n
+
