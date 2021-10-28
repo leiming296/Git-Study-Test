eb51e8e7f091435fccaffb9d5962d2b16002ee92
DESKTOP-S2EDBCG\ming <lei.ming296@gmail.com>
2021-10-28 14:51:34 -0700

    bug 2 problem

--- predictions.py
+++ predictions.py
@@ -10,14 +10,14 @@
 def ml_predictions(n):
     n = n + 1
     print('from bug2')
-    print('from commit 2')
+    print('from commit 2, from bug 2 branch')
     return n
 
 def ml_data(m):
-    print('from commit 2')
+    print('from commit 2, from bug 2 branch')
     return m
 
 def m3_data(n):
-    print('from bug 2')
+    print('from bug 2, from bug 2 branch')
     return n
 
